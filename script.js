f264b7b8815703659323203b57a39f7c51837784
Josh Wayles <Joshwayles@gmail.com>
2024-05-27 11:38:20 +0100

    Adding files
    Uploading the counter app files.

---
+++ script.js
@@ -0,0 +1,58 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+document.getElementById("increase-btn").addEventListener("click", function(){
+  increaseBtn()
+})
+function increaseBtn(){
+  count += 1
+  countEl.textContent = count
+}
+
+document.getElementById("save-btn").addEventListener("click", function(){
+  saveBtn()
+})
+function saveBtn(){
+  if(saveEl.textContent === "_"){
+    saveEl.textContent = count
+    count = 0
+    countEl.textContent = count
+  }
+  else{
+    saveEl.textContent = count + " ~ " + saveEl.textContent
+    count = 0
+    countEl.textContent = count
+  }
+}
+
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  document.getElementById('current-time').textContent = "Current Time: " + h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+document.getElementById("time-btn").addEventListener("click", function(){
+  saveTime() 
+})
+function saveTime(){
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  const timeNow = h + ":" + m + ":" + s;
+  if(saveEl.textContent === "_"){
+  saveEl.textContent = count + " @ " + timeNow
+  count = 0
+   countEl.textContent = count
+  }
+  else{
+  saveEl.textContent = count + " @ " + timeNow + " ~ " + saveEl.textContent
+  count = 0
+  countEl.textContent = count 
+  }
+}
