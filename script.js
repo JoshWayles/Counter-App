8f8ce1f4d3cb74a9db3c0e9243f34855a70dcb07
Josh Wayles <Joshwayles@gmail.com>
2024-06-26 15:16:05 +0100

    Formating and accessibility edits

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-let totalEl =document.getElementById("total-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
 let total = 0
 
-document.getElementById("increase-btn").addEventListener("click", function(){
+document.getElementById("increase-btn").addEventListener("click", function () {
   increaseBtn()
 })
-function increaseBtn(){
+function increaseBtn() {
   count += 1
   countEl.textContent = count
 }
 
-document.getElementById("save-btn").addEventListener("click", function(){
+document.getElementById("save-btn").addEventListener("click", function () {
   saveBtn()
 })
-function saveBtn(){
+function saveBtn() {
 
   total += count
   totalEl.textContent = "Total: " + total
 
-  if(saveEl.textContent === "_"){
+  if (saveEl.textContent === "_") {
     saveEl.textContent = count
     count = 0
     countEl.textContent = count
   }
-  else{
+  else {
     saveEl.textContent = count + " ~ " + saveEl.textContent
     count = 0
     countEl.textContent = count
@@ -39,7 +39,7 @@ function startTime() {
 
   let timeNow = ""
 
-  if(m < 10){
+  if (m < 10) {
     timeNow = h + ":" + "0" + m;
   }
   else {
@@ -50,35 +50,35 @@ function startTime() {
   setTimeout(startTime, 1000);
 }
 
-document.getElementById("time-btn").addEventListener("click", function(){
-  saveTime() 
+document.getElementById("time-btn").addEventListener("click", function () {
+  saveTime()
 })
-function saveTime(){
+function saveTime() {
 
   total += count
   totalEl.textContent = "Total: " + total
-  
+
   const today = new Date();
   let h = today.getHours();
   let m = today.getMinutes();
-  
+
   let timeNow = ""
 
-  if(m < 10){
+  if (m < 10) {
     timeNow = h + ":" + "0" + m;
   }
   else {
     timeNow = h + ":" + m;
   }
-  
-  if(saveEl.textContent === "_"){
-  saveEl.textContent = count + " @ " + timeNow
-  count = 0
-   countEl.textContent = count
+
+  if (saveEl.textContent === "_") {
+    saveEl.textContent = count + " @ " + timeNow
+    count = 0
+    countEl.textContent = count
   }
-  else{
-  saveEl.textContent = count + " @ " + timeNow + " ~ " + saveEl.textContent
-  count = 0
-  countEl.textContent = count 
+  else {
+    saveEl.textContent = count + " @ " + timeNow + " ~ " + saveEl.textContent
+    count = 0
+    countEl.textContent = count
   }
 }
